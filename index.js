d866407200481f5e857d0b91ae21575f9b1b0899
Matt Bullen <burner0110@gmail.com>
2015-08-24 15:52:42 -0700

    .

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ wss.on("connection", function(ws) {
     
     console.log("Websocket: open");
     
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function() {});
-    }, 5000);
+    //var id = setInterval(function() {
+    //    ws.send(JSON.stringify(new Date()), function() {});
+    //}, 5000);
     
-    wss.on("message", function(data, id) {
+    wss.on("message", function(data) {
         console.log("Data:", data);
-        var msg = wss.unmaskMessage(data);
-        console.log(wss.convertToString("Message received:", msg.opcode, msg.message));
+        //var msg = wss.unmaskMessage(data);
+        //console.log(wss.convertToString("Message received:", msg.opcode, msg.message));
         
-        var packagedMessage = wss.packageMessage(msg.opcode, msg.message);
-        wss.sendMessage("all", packagedMessage);
-        //ws.sendMessage(packagedMessage);
+        //var packagedMessage = wss.packageMessage(msg.opcode, msg.message);
+        //wss.sendMessage("all", packagedMessage);
+        ws.send(JSON.stringify(data));
     });
 
     ws.on("close", function() {
