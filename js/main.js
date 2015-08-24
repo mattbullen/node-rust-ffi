dd75df43031a897b559dc3291af6da8a6099e06b
Matt Bullen <burner0110@gmail.com>
2015-08-24 16:11:48 -0700

    .

--- js/main.js
+++ js/main.js
@@ -12,9 +12,10 @@ function init() {
     var host = location.origin.replace(/^http/, "ws");
     var ws = new WebSocket(host);
     ws.onmessage = function(event) {
+        console.log("Reply:", event.data);
         var li = document.createElement("li");
-        li.innerHTML = JSON.parse(event.data);
-        document.querySelector("#result").appendChild(li);
+        li.innerHTML = event.data;
+        document.querySelector("#list").appendChild(li);
     };
      
     $("#button").on("click", function(event) {
