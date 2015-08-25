f291a5a209348deeeb52896c9e1432b53155d890
Matt Bullen <burner0110@gmail.com>
2015-08-24 17:21:53 -0700

    .

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require("http");
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 5000;
+var ffi = require("ffi");
 
 app.use(express.static(__dirname + "/"));
 
@@ -19,8 +20,16 @@ wss.on("connection", function(ws) {
     
     ws.on("message", function(data) {
         console.log("Message received:", data);
+        
+        var ffiLibrary = ffi.Library("cc", {
+            how_many_characters: ["uint32", ["string"]],
+        });
+        var chars = ffiLibrary.how_many_characters(data);
+        console.log("FFI:", chars);
+        
         var reply = {
             message: data,
+            count: chars,
             status: "OK"
         }
         ws.send(JSON.stringify(reply));
