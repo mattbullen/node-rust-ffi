8e5c3849833ff306d7a48935bef0afae03dba969
Matt Bullen <burner0110@gmail.com>
2015-08-24 19:12:55 -0700

    .

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
+// Configuration settings
 var WebSocketServer = require("ws").Server;
 var http = require("http");
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 5000;
 var ffi = require("ffi");
-
 app.use(express.static(__dirname + "/"));
 
+// Start the server
 var server = http.createServer(app);
 server.listen(port);
 console.log("HTTP server listening on port %d", port);
 
+// Start the websocket server
 var wss = new WebSocketServer({server: server});
 console.log("Websocket: created");
 
+// Message counter: resets on new connection for this example
+var messageCount = 0;
+
+// Define websocket events
 wss.on("connection", function(ws) {
     
     console.log("Websocket: open");
     
     ws.on("message", function(data) {
+        
         console.log("Message received:", data);
         
         var ffiLibrary = ffi.Library("cc", {
@@ -28,16 +35,21 @@ wss.on("connection", function(ws) {
         console.log("FFI:", chars);
         
         var reply = {
+            id: messageCount,
             message: data,
             count: chars,
+            date: new Date().toLocaleString(),
             status: "OK"
         }
         ws.send(JSON.stringify(reply));
         console.log("Reply sent:",reply);
+        
+        messageCount++;
     });
 
     ws.on("close", function() {
         console.log("Websocket: closed");
+        messageCount = 0;
     })
     
 });
